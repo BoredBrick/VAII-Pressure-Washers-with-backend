2f970ed8cf1295258f2362775d51f2732127fc42
BoredBrick <67233864+BoredBrick@users.noreply.github.com>
2022-01-09 11:42:54 +0100

    Cleaned us CSS

--- newsletter.php
+++ newsletter.php
@@ -22,7 +22,7 @@ $app = new App();
     />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/newsletter.css" />
     <script type = "text/javascript" src="js/script.js"></script>
 
     <title>PowerWashers</title>
